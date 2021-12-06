34067204a8e018ceb4f4cb488be2c92a380dd84c
daibhin <david@monaru.io>
2021-12-06 11:20:13 +0000

    Update sample UIKit integration

--- samples/UIKit_CocoaPods/UIKit_CocoaPods/ViewController.swift
+++ samples/UIKit_CocoaPods/UIKit_CocoaPods/ViewController.swift
@@ -6,9 +6,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .cyan
-        
+
         let unflowViewController = OpenerViewController()
+        addChild(unflowViewController)
         view.addSubview(unflowViewController.view)
+        unflowViewController.didMove(toParent: self)
     }
 }
 
