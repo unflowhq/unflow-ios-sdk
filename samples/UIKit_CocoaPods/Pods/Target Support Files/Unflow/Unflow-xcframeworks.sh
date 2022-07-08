52508a95b9fc50eb90d53ccd737faa1b8abbf6ab
Alex Logan <me@alexanderlogan.co.uk>
2022-07-08 10:18:09 +0100

    Update cocoapod sample to 1.9.3

--- samples/UIKit_CocoaPods/Pods/Target Support Files/Unflow/Unflow-xcframeworks.sh
+++ samples/UIKit_CocoaPods/Pods/Target Support Files/Unflow/Unflow-xcframeworks.sh
@@ -17,16 +17,10 @@ RSYNC_PROTECT_TMP_FILES=(--filter "P .*.??????")
 variant_for_slice()
 {
   case "$1" in
-  "UnflowUI.xcframework/ios-arm64_x86_64-simulator")
-    echo "simulator"
-    ;;
   "UnflowUI.xcframework/ios-arm64")
     echo ""
     ;;
-  "Unflow.xcframework/ios-arm64")
-    echo ""
-    ;;
-  "Unflow.xcframework/ios-arm64_x86_64-simulator")
+  "UnflowUI.xcframework/ios-arm64_x86_64-simulator")
     echo "simulator"
     ;;
   esac
@@ -35,16 +29,10 @@ variant_for_slice()
 archs_for_slice()
 {
   case "$1" in
-  "UnflowUI.xcframework/ios-arm64_x86_64-simulator")
-    echo "arm64 x86_64"
-    ;;
   "UnflowUI.xcframework/ios-arm64")
     echo "arm64"
     ;;
-  "Unflow.xcframework/ios-arm64")
-    echo "arm64"
-    ;;
-  "Unflow.xcframework/ios-arm64_x86_64-simulator")
+  "UnflowUI.xcframework/ios-arm64_x86_64-simulator")
     echo "arm64 x86_64"
     ;;
   esac
@@ -129,6 +117,5 @@ install_xcframework() {
   echo "Copied $source to $destination"
 }
 
-install_xcframework "${PODS_ROOT}/Unflow/UnflowUI.xcframework" "Unflow" "framework" "ios-arm64_x86_64-simulator" "ios-arm64"
-install_xcframework "${PODS_ROOT}/Unflow/Unflow.xcframework" "Unflow" "framework" "ios-arm64" "ios-arm64_x86_64-simulator"
+install_xcframework "${PODS_ROOT}/Unflow/UnflowUI.xcframework" "Unflow" "framework" "ios-arm64" "ios-arm64_x86_64-simulator"
 
